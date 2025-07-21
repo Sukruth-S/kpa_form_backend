7f7e0e4c4497ba1790bd5597b76a20ec6c3c7f4b
Sukruth S <119930076+Sukruth-S@users.noreply.github.com>
2025-07-21 22:50:51 +0530

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -3,21 +3,19 @@ from app import models
 from app import schemas
 
 
-# Create new form data entry
 def create_form_data(db: Session, form_data: schemas.FormDataCreate):
     db_form = models.FormData(
         full_name=form_data.full_name,
         phone_number=form_data.phone_number,
         email=form_data.email,
         address=form_data.address,
-        resume_file=form_data.resume_file  # <-- use the value from schema
+        resume_file=form_data.resume_file  
     )
     db.add(db_form)
     db.commit()
     db.refresh(db_form)
     return db_form
 
-# Get form data by ID
 def get_form_data(db: Session, form_id: int):
     return db.query(models.FormData).filter(models.FormData.id == form_id).first()
 
